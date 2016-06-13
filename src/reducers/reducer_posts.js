7a0d954c0376ff7da7003c856f479c261b27a953
tommy.crafton <tommy.crafton@ruralsourcing.com>
2016-06-13 16:01:49 -0500

    Added code to post, view and delete blogs

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,16 @@
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+
+const INITIAL_STATE = { all: [], post: null };
+
+export default function (state = INITIAL_STATE, action){
+  switch (action.type) {
+    case FETCH_POSTS:
+      return { ...state, all: action.payload.data };
+      break;
+    case FETCH_POST:
+      return { ...state, post: action.payload.data };
+      break;
+    default:
+      return state;
+  }
+}
